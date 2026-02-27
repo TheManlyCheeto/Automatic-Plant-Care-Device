67c8357c421bc7f6435bbeaaba6bf2a25383d1ba
TheManlyCheeto <bewers20@gmail.com>
2026-02-27 15:30:13 -0700

    add soil sensor test scipt

---
+++ Soili2cTest.py
@@ -0,0 +1,12 @@
+import time 
+import board 
+import busio
+from adafruit_seesaw.seesaw import Seesaw
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = Seesaw(i2c, addr=0x36)
+
+while True:
+    print("moisture", sensor.moisture_read(),
+          "temp (C):", round(sensor.get_temp(), 2))
+    time.sleep(1)
