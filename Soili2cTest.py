4a768c81fbea9344dc852e6e3beb35704d94e6ea
TheManlyCheeto <bewers1@uwyo.edu>
2026-02-27 15:41:19 -0700

    added comments to test file

--- Soili2cTest.py
+++ Soili2cTest.py
@@ -2,7 +2,7 @@ import time
 import board 
 import busio
 from adafruit_seesaw.seesaw import Seesaw
-
+# basic test file for Soil Sensor.
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = Seesaw(i2c, addr=0x36)
 
