ae0e4d853bfa976c217dbfa939443682f54a617c
TheManlyCheeto <bewers1@uwyo.edu>
2026-04-21 18:56:53 -0600

    added plots and ultrasonic sensor test

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -3,7 +3,7 @@ import time
 # Voltage Divider on the Echo pin.
 # ECHO pin -> 1kohm -> GPIO 24 -> 2kohm -> GND
 
-TRIG_PIN = 23
+TRIG_PIN = 26
 ECHO_PIN = 24
 
 GPIO.setmode(GPIO.BCM)
@@ -22,19 +22,23 @@ def measure_distance() -> float:
     # Wait for echo to start
     pulse_start = time.time()
     while GPIO.input(ECHO_PIN) == 0:
+        #print("wait fore echo to start")
         pulse_start = time.time()
 
     # Wait for echo to end
     pulse_end = time.time()
     while GPIO.input(ECHO_PIN) == 1:
+        #print("Wait for echo to end")
         pulse_end = time.time()
 
     # Calculate distance
     pulse_duration = pulse_end - pulse_start
     distance = (pulse_duration * 34300) / 2  # speed of sound in cm/s
+    print(f"distance: {distance}")
     return round(distance, 2)
 
 try:
+    print("while is running")
     while True:
         dist = measure_distance()
         plant_growth = 24.53 - dist # distance from ultrasonic sensor to the dirt level
